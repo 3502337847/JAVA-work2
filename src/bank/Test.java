771d71e2f0abf0fc9720a88b1349166cc978c62e
王梦凡 <@DESKTOP-70ON0P7>
2017-10-14 00:16:04 +0800

    java作业2

---
+++ src/bank/Test.java
@@ -0,0 +1,17 @@
+package bank;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DepositAccount saver = new DepositAccount("1234567890","zhangsan",5000);
+		saver.setRate(0.023);//设置年利率
+		saver.Deposit(2000);//存入2000元
+		saver.queryLeft();//查询余额
+		System.out.println("年利息是"+saver.calYearRate());
+
+	}
+
+}
+
